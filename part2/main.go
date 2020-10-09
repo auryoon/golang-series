f3a93456831bc9458d944cf859c0672a13394ac0
Auryon <auryon.id@gmail.com>
2020-10-09 21:28:47 +0800

    add part 2

---
+++ part2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string
+	nama = "Auryon"
+	fmt.Println("Halo namaku", nama)
+	nama = "AuryonUnyuUnyu"
+	fmt.Println("Halo namaku berubah menjadi", nama)
+
+	const umur = 99999
+	fmt.Println("Halo umur ku", umur)
+	// umur = 1
+	fmt.Println("Halo umur ku berubah menjadi", umur)
+
+	// nama = 0
+
+	// fmt.Println([]rune(nama))
+
+	// var husbando string
+	// var umur int
+	// var jawaban bool
+	// var float float64
+	// fmt.Println(husbando, umur, jawaban, float)
+
+}
